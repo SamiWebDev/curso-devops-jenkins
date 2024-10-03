a1d169b6916ebacc2e088fb1cd6c6e0d1da45b37
samito4 <sami.mimun1@gmail.com>
2024-10-03 16:53:33 +0200

    fly deploy 4

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 // Ruta básica
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World! From Sami Mimun! CICD 1" });
+  res.status(200).json({ message: "Hello World! From Sami Mimun! CICD 2" });
 });
 
 // Ruta de ejemplo para pruebas
